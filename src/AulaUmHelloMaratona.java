5ebdf8bc1c4c664c100fdbed8b2bde12aceb2d15
ecvunge <edvaldocv@ext.inditex.com>
2022-03-05 14:54:49 +0000

    criação da primeira class

---
+++ src/AulaUmHelloMaratona.java
@@ -0,0 +1,6 @@
+public class AulaUmHelloMaratona {
+    public static void main(String[] args) {
+        System.out.println("ola, maratona do jiraya");
+        System.out.println("by: devDojo Academy.");
+    }
+}
